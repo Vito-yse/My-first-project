ede636e8725e7983962859e18458c4e9943b6130
Vitor <146575009+Vito-yse@users.noreply.github.com>
2023-10-01 17:28:50 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const botoes = document.querySelectorAll(".botao");
+
+const personagens = document.querySelectorAll(".personagem")
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+
+        desselecionarBotao();
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado")
+        personagens[indice].classList.add("selecionado");
+    });
+});
+
+function desselecionarPersonagem() {
+    const personagemselecionado = document.querySelector(".personagem.selecionado");
+
+    personagemselecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoselecionado = document.querySelector(".botao.selecionado");
+
+    botaoselecionado.classList.remove("selecionado");
+}
